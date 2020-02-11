3408588a438908375899744235670e7129d3e428
unknown <lyub-belikova@yandex.ru>
2020-02-11 21:52:03 +0300

    add text preprocessing code

---
+++ preprocess_corpus.py
@@ -0,0 +1,66 @@
+from json_functions import *
+import pandas as pd
+from pymystem3 import Mystem
+mystem = Mystem() 
+
+mfa_texts = get_json_from_file('mfa_texts.json')
+
+# transform neccessary texts & their metadata to list of lists
+# structure: title | date | category | header (if any) |  text
+
+data = [] 
+for category, texts in mfa_texts.items():
+    if category not in ['answers']:
+        for text in texts:
+            text_data = []
+            text_data.append(text['title'])
+            text_data.append(text['date'])
+            text_data.append(category)
+            if 'text' in text:
+                text_data.append(('nan'))
+                text_data.append(text['text'])
+                data.append(text_data)
+            else:
+                for text_block in text['text_blocks']:
+                    text_data_copy = text_data.copy()
+                    if text_block['type'] == 'speech':
+                        if 'title' in text_block:
+                            text_data_copy.append(text_block['title'])
+                        else:
+                            text_data_copy.append(float('nan'))
+                        text_data_copy.append(text_block['text'])
+                        data.append(text_data_copy)
+                    else:
+                        break
+
+df = pd.DataFrame(data, columns = ['title', 'date', 'category', 'subheading', 'text'])
+df.to_csv('mfa_texts_df.csv', sep='\t', encoding='utf-8')
+
+# functions for text preprocessing
+
+def get_all_texts(df):
+    all_texts = ''
+    for text in df['text']:
+        all_texts += text
+    return all_texts
+
+def generate_symbols_to_del(text):
+    symbols_to_del = []
+    for symbol in set(text):
+        if symbol.isalpha() == False:
+            symbols_to_del.append(symbol)
+    return symbols_to_del
+
+def lemmatize(text):
+    return ''.join(mystem.lemmatize(text))
+
+def clean(text, symbols_to_del):
+    for symbol_to_del in symbols_to_del:
+        text = text.replace(symbol_to_del, ' ')
+    text = ' '.join([symbol for symbol in text.split() if symbol != ' '])
+    return text
+
+# symbols_to_del = generate_symbols_to_del(get_all_texts(df))
+    
+df['text_lemmatized'] = df['text'].apply(lemmatize)
+df.to_csv('mfa_texts_df.csv', sep='\t', encoding='utf-8')
